617771d8ed8caecad6de960026b17d4fbe107117
Niko <nicolas.rojasp@upb.edu.co>
2025-10-29 08:35:16 -0500

    aw

--- TrabajoFinalFP/TrabajoFinalFP/Program.cs
+++ TrabajoFinalFP/TrabajoFinalFP/Program.cs
@@ -26,10 +26,19 @@
              *Salir del programa    - Niko */
 
         public static string[,] listaVehiculos;
+        public static string[] listaPlacas = new string[20];
+        public static string[,] listaClientes;
+        public static string[,] listaServicios;
+        public static bool salir = false;
         static void Main(string[] args)
         {
             listaVehiculos = CrearMatriz();
-            MenuPrincipal();
+            while (!salir)
+            {
+                MenuPrincipal();
+
+            }
+            SalirDelPrograma();
         }
 
         static void MenuPrincipal()
@@ -46,28 +55,26 @@
 
                 //case 3: GestionDeServicios(); break;
 
-                case 4: SalirDelPrograma(); break;
+                case 4: salir = true; break;
             }
         }
 
+        // Gestión de Vehículos ===================================================
         static void GestionDeVehiculos(string[,] listaVehiculos)
         {
             Console.WriteLine($"Escoge una opción \n1. Registro \n2. Ver Lista de Vehículos");
-            switch (Convert.ToInt32(Console.ReadLine()))
+            int rta = Convert.ToInt32(Console.ReadLine());
+            switch (rta)
             {
+                case 0:
+                    Console.WriteLine("Ingresa una respuesta válida"); break;
                 case 1:
                     listaVehiculos = Registro(listaVehiculos); break;
                 case 2: 
                     MostrarMatriz(listaVehiculos); break;
             }
+            LimpiarConsola();
         }
-
-        static string[,] CrearMatriz()
-        {
-            string[,] Registro = new string[20,4];
-            return Registro;
-        }
-
         static string[,] Registro(string[,] matriz)
         {
             Console.WriteLine("Ingresa la placa porfa");
@@ -78,12 +85,14 @@
             string marca = ObtenerString();
             Console.WriteLine("Ingresa el año de tu carro");
             string año = ObtenerString();
-
+            
+            // Columnas: 0. Placa | 1. Modelo |  2. Marca | 3. Año
             for (int i = 0; i < matriz.GetLength(0); i++)
             {
                 if (matriz[i,0] == null)
                 {
                     matriz[i, 0] = placa;
+                    break;
                 }
                 else
                     continue;
@@ -93,6 +102,7 @@
                 if (matriz[i, 1] == null)
                 {
                     matriz[i, 1] = modelo;
+                    break;
                 }
                 else
                     continue;
@@ -102,6 +112,7 @@
                 if (matriz[i, 2] == null)
                 {
                     matriz[i, 2] = marca;
+                    break;
                 }
                 else
                     continue;
@@ -111,39 +122,125 @@
                 if (matriz[i, 3] == null)
                 {
                     matriz[i, 3] = año;
+                    break;
                 }
                 else
                     continue;
             }
 
+            LimpiarConsola();
             return matriz;
         }
 
+        // Gestión de Servicios ===================================================
+        public void GestionDeServicios(string[,] listaVehiculos)
+        {
+            Console.WriteLine("[Gestion de servicios]");
+            Console.WriteLine("Escoja una de las siguientes opciones: \n1. Registro de servicios \n2. Historial de servicios \n3. Resumen de servicios \n4. Menu Principal");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1: RegistroServicio(); break;
+            }
+        }
+        public void RegistroServicio()
+        {
+            ObtenerPlacas();
+            Console.WriteLine("Selecciona el vehículo al que quieres registrarle un servicio (1-20)");
+            int vehiculoElegido = ElegirVehiculo();
+            Console.WriteLine("Ingresa el nombre del servicio a registrar");
+            string servicioElegido = ObtenerString();
+            Console.WriteLine("Ingresa la fecha en la que se está registrando el servicio");
+            string fecha = ObtenerString();
+            Console.WriteLine("Ingresa el precio del servicio elegido");
+            string precio = ObtenerString();
+            for (int i = 0; i < listaPlacas.Length; i++)
+            {
+                if (vehiculoElegido - 1 == i)
+                {
+                    string placaElegida = listaPlacas[i];
+                }
+            }
+        }
+        public void ObtenerPlacas()
+        {
+            if (listaVehiculos == null)
+            {
+                Console.WriteLine("Error: La lista de vehículos está vacía");
+                return;
+            }
+
+            for (int i = 0; i < listaVehiculos.GetLength(0); i++)
+            {
+                listaPlacas[i] = listaVehiculos[i, 0];
+                Console.WriteLine($"{i + 1} {listaPlacas[i]}");
+            }
+        }
+
+        static void GenerarMatrizServicios(string placa, string servicio, string fecha, string precio)
+        {
+            for (int i = 0; i < listaPlacas.Length; i++)
+            {
+                for (int j  = 0; j < 4; j++)
+                {
+                    //listaServicios()
+                }
+            }
+        }
+        static int ElegirVehiculo()
+        {
+            int rta = Convert.ToInt32(Console.ReadLine);
+            bool rtaValida = false;
+            while (!rtaValida)
+            {
+                if (0 < rta && rta <= 20)
+                {
+                    rtaValida = true;
+                    rta = Convert.ToInt32(Console.ReadLine);
+                }
+                else
+                    Console.WriteLine("Por favor ingresa un valor válido");
+            }
+            return rta;
+        }
+        // Matriz ==================================================================
         static void MostrarMatriz(string[,] matriz)
         {
             for(int i = 0; i<matriz.GetLength(0); i++)
             {
                 for (int j = 0; j < matriz.GetLength(1); j++)
                 {
-                    Console.Write(matriz[i, j]);
+                    Console.Write(matriz[i, j] + " ");
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+            LimpiarConsola();
+        }
+        static string[,] CrearMatriz()
+        {
+            string[,] Registro = new string[20,4];
+            return Registro;
         }
 
+        // Obtención de valores ===================================================
         static string ObtenerString()
         {
             string palabra = Console.ReadLine();
             return palabra;
         }
-
         static int ObtenerNumero()
         {
             int numero = int.Parse(Console.ReadLine());
             return numero;
         }
 
-       static void SalirDelPrograma()
+        // Misceláneos ============================================================
+        static void LimpiarConsola()
+        {
+            Console.Clear();
+        }
+        static void SalirDelPrograma()
         {
             System.Environment.Exit(0);
         }
